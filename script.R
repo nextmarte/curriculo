90e4b384a69855b6160747924cc04c207ea522d3
nextmarte <marcusantonio@id.uff.br>
2023-06-26 16:59:14 -0300

    first

---
+++ script.R
@@ -0,0 +1,9 @@
+library(googlesheets4)
+
+datadrivencv::use_datadriven_cv(
+  full_name = "Marcus Ramalho",
+  data_location = "https://docs.google.com/spreadsheets/d/1zPVICSel5EB9M6d8bhhpVpBk5kDIrX_u8HDScEa-oYg/edit?usp=sharing",
+  pdf_location = "https://github.com/nextmarte/curriculo/raw/master/Marcus_Ramalho_strayer_cv.pdf",
+  html_location = "https://github.com/nextmarte/Marcus_Ramalho_CV.github.io/",
+  source_location = "https://github.com/nextmarte/curriculo"
+)
